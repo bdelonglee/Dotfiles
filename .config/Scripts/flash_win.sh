f03fabe019f8f36eeec12e4f129d253e7718128b
bdelonglee <bdelonglee@gmail.com>
2018-02-07 20:31:52 -0500

    flash_win modified with transset-df

--- .config/Scripts/flash_win.sh
+++ .config/Scripts/flash_win.sh
@@ -4,10 +4,10 @@
 
 #Requires transset-df and a composite manager, like xcompmgr.
 
-transset -a -m 0
+transset-df -a -m 0
 sleep .1
-transset -a -x 1
+transset-df -a -x 1
 sleep .1
-transset -a -m 0
+transset-df -a -m 0
 sleep .1
-transset -a -x 1
+transset-df -a -x 1
